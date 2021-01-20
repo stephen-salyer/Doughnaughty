46921af39def683ccb0821195ec952ca22fab2b6
Stephen Salyer <39383255+stephen-salyer@users.noreply.github.com>
2021-01-20 14:29:57 -0600

    hero complete

--- src/components/Doughnuts.js
+++ src/components/Doughnuts.js
@@ -1,35 +1,127 @@
 import React from "react";
-import { Box } from "@material-ui/core";
-import HeroLeft from "../images/Hero-Image-Doughnuts-Left.png";
+import { Box, Hidden } from "@material-ui/core";
+import HeroLeft1 from "../images/Hero-Image-Doughnuts-Left-1.png";
+import HeroLeft2 from "../images/Hero-Image-Doughnuts-Left-2.png";
 import HeroRight from "../images/Hero-Image-Doughnuts-Right.png";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+
+const useStyles = makeStyles({
+  "@keyframes spin": {
+    "0%": {
+      transform: "rotate(0deg)",
+    },
+    "100%": {
+      transform: "rotate(360deg)",
+    },
+  },
+  selector: {
+    animation: "$spin 50s linear infinite",
+  },
+  "@keyframes spinRight": {
+    "0%": {
+      transform: "rotate(0deg)",
+    },
+    "100%": {
+      transform: "rotate(360deg)",
+    },
+  },
+  selectorRight: {
+    animation: "$spin 50s linear infinite reverse",
+  },
+});
 
 export default function Doughnuts() {
+  const classes = useStyles();
+
   return (
     <Box style={{ display: "flex", justifyContent: "space-between" }}>
-      <div>
-        <img
-          src={HeroLeft}
-          alt="doughnuts"
-          style={{
-            width: 600,
-            marginLeft: -100,
-            marginTop: -360,
-            position: "fixed",
-          }}
-        />
-      </div>
-      <div>
-        <img
-          src={HeroRight}
-          alt="doughnuts again"
-          style={{
-            width: 450,
-            marginRight: 100,
-            marginTop: -350,
-            position: "sticky",
-          }}
-        />
-      </div>
+      <Hidden smDown>
+        <Box>
+          <div>
+            <img
+              className={clsx(classes.selector)}
+              src={HeroLeft1}
+              alt="doughnuts"
+              style={{
+                width: 250,
+                marginLeft: 300,
+                marginTop: -350,
+                position: "fixed",
+              }}
+            />
+          </div>
+          <div>
+            <img
+              className={clsx(classes.selector)}
+              src={HeroLeft2}
+              alt="doughnuts"
+              style={{
+                width: 300,
+                marginLeft: 0,
+                marginTop: -200,
+                position: "fixed",
+              }}
+            />
+          </div>
+        </Box>
+        <div>
+          <img
+            src={HeroRight}
+            className={clsx(classes.selectorRight)}
+            alt="doughnuts again"
+            style={{
+              width: 450,
+              marginRight: 100,
+              marginTop: -350,
+              position: "sticky",
+            }}
+          />
+        </div>
+      </Hidden>
+      <Hidden mdUp>
+        <Box>
+          <div>
+            <img
+              className={clsx(classes.selector)}
+              src={HeroLeft1}
+              alt="doughnuts"
+              style={{
+                width: 150,
+                marginLeft: 100,
+                marginTop: -350,
+                position: "fixed",
+              }}
+            />
+          </div>
+          <div>
+            <img
+              className={clsx(classes.selector)}
+              src={HeroLeft2}
+              alt="doughnuts"
+              style={{
+                width: 200,
+                marginLeft: -100,
+                marginTop: -200,
+                position: "fixed",
+              }}
+            />
+          </div>
+        </Box>
+        <div>
+          <img
+            src={HeroRight}
+            className={clsx(classes.selectorRight)}
+            alt="doughnuts again"
+            style={{
+              width: 250,
+              marginRight: -100,
+              marginTop: -450,
+              position: "sticky",
+            }}
+          />
+        </div>
+      </Hidden>
     </Box>
   );
 }
--- src/components/HeroInfo.js
+++ src/components/HeroInfo.js
@@ -1,19 +1,19 @@
 import {
-  AppBar,
-  Avatar,
   Box,
   Button,
   Container,
   IconButton,
   InputBase,
   Paper,
-  TextField,
-  Toolbar,
   Typography,
+  Link,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { GpsFixed, LocationOn, LocationSearching } from "@material-ui/icons";
+import { GpsFixed, LocationOn } from "@material-ui/icons";
 import React from "react";
+import grubhub from "../images/grubhub-logo.png";
+import doordash from "../images/doordash-logo.png";
+import postmates from "../images/postmates-Logo.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,6 +33,12 @@ const useStyles = makeStyles((theme) => ({
     height: 28,
     margin: 4,
   },
+  margins: {
+    marginTop: -350,
+    [theme.breakpoints.down("sm")]: {
+      marginTop: -310,
+    },
+  },
 }));
 
 const locations = [
@@ -42,11 +48,35 @@ const locations = [
   { city: "Clayton" },
 ];
 
+const logos = [
+  {
+    logo: doordash,
+    alt: "Doordash-Logo",
+    width: 86,
+    height: 60,
+    link: "https://www.doordash.com/en-US",
+  },
+  {
+    logo: postmates,
+    alt: "Postmates-Logo",
+    width: 41,
+    height: 40,
+    link: "https://postmates.com/",
+  },
+  {
+    logo: grubhub,
+    alt: "Grubhub-Logo",
+    width: 86,
+    height: 61,
+    link: "https://www.grubhub.com/lets-eat",
+  },
+];
+
 export default function HeroInfo() {
   const classes = useStyles();
 
   return (
-    <Container maxWidth="md" style={{ marginTop: -320 }}>
+    <Container maxWidth="md" className={classes.margins}>
       <Box style={{ position: "relative" }}>
         <Box
           style={{
@@ -60,7 +90,7 @@ export default function HeroInfo() {
           <Typography variant="h3" component="h3">
             We Deliver
           </Typography>
-          <Typography variant="body1" component="body1">
+          <Typography variant="body1">
             Find out if we deliver to you!
           </Typography>
         </Box>
@@ -102,7 +132,6 @@ export default function HeroInfo() {
             {locations.map(({ city }) => (
               <Typography
                 variant="body2"
-                component="body2"
                 color="textSecondary"
                 style={{ marginRight: 8 }}
               >
@@ -110,7 +139,26 @@ export default function HeroInfo() {
               </Typography>
             ))}
           </Box>
-          <Box></Box>
+          <Box
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              paddingTop: 8,
+            }}
+          >
+            {logos.map(({ logo, alt, width, height, link }) => (
+              <Link href={link} style={{ paddingRight: 8 }}>
+                <Button>
+                  <img
+                    src={logo}
+                    alt={alt}
+                    style={{ width: width, height: height }}
+                  />
+                </Button>
+              </Link>
+            ))}
+          </Box>
         </Box>
       </Box>
     </Container>
