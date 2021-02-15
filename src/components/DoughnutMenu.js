5037ef5fb3f1f14be6bbe980eb13d4272ce4cf02
Stephen Salyer <39383255+stephen-salyer@users.noreply.github.com>
2021-02-15 13:48:42 -0600

    Tabs

--- src/components/DoughnutMenu.js
+++ src/components/DoughnutMenu.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import { Container } from "@material-ui/core";
+import { withStyles, makeStyles, useTheme } from "@material-ui/core/styles";
+import { Container, Typography, Box, Tabs, Tab } from "@material-ui/core";
+import SwipeableViews from "react-swipeable-views";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -53,24 +51,66 @@ const AntTab = withStyles((theme) => ({
   selected: {},
 }))((props) => <Tab disableRipple {...props} />);
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
+
 export default function DoughnutMenu() {
   const classes = useStyles();
+  const theme = useTheme();
   const [value, setValue] = React.useState(0);
-
   const handleChange = (event, newValue) => {
     setValue(newValue);
+  };
+  const handleChangeIndex = (index) => {
+    setValue(index);
   };
 
   return (
     <Container disableGutters maxWidth="lg" className={classes.container}>
-      <div className={classes.demo1}>
-        <AntTabs value={value} onChange={handleChange} aria-label="ant example">
-          <AntTab label="Tab 1" />
-          <AntTab label="Tab 2" />
-          <AntTab label="Tab 3" />
-        </AntTabs>
-        <Typography className={classes.padding} />
-      </div>
+      <AntTabs value={value} onChange={handleChange} aria-label="ant example">
+        <AntTab label="Tab 1" {...a11yProps(0)} />
+        <AntTab label="Tab 2" {...a11yProps(1)} />
+        <AntTab label="Tab 3" {...a11yProps(2)} />
+      </AntTabs>
+      <SwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          Item Three
+        </TabPanel>
+      </SwipeableViews>
     </Container>
   );
 }
